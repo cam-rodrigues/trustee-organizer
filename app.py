205a21f274973e21b995003544b91df1620225c1
Cam <219443440+cam-rodrigues@users.noreply.github.com>
2026-06-04 15:19:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,40 @@ st.set_page_config(
 )
 
 st.title("Trustee Organizer")
+
+# Top Row
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.container(border=True, height=120)
+
+with col2:
+    st.container(border=True, height=120)
+
+with col3:
+    st.container(border=True, height=120)
+
+with col4:
+    st.container(border=True, height=120)
+
+st.write("")
+
+# Middle Row
+left, right = st.columns([2, 1])
+
+with left:
+    st.container(border=True, height=400)
+
+with right:
+    st.container(border=True, height=400)
+
+st.write("")
+
+# Bottom Row
+col1, col2 = st.columns(2)
+
+with col1:
+    st.container(border=True, height=300)
+
+with col2:
+    st.container(border=True, height=300)
